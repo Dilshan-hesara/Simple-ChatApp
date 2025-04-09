ee89bf87d588ad8d34509cd89a01993ba34ac644
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-04-09 09:52:05 +0530

    server conect

--- src/main/java/lk/cw/simplechatapp/controller/serverConrroller.java
+++ src/main/java/lk/cw/simplechatapp/controller/serverConrroller.java
@@ -1,11 +1,18 @@
 package lk.cw.simplechatapp.controller;
 
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class serverConrroller {
 
@@ -18,9 +25,47 @@ public class serverConrroller {
     @FXML
     private TextField txtmsg;
 
-    @FXML
-    void sendbtnOnAction(ActionEvent event) {
+    private DataInputStream input;
+    private DataOutputStream output;
 
+    public void initialize() {
+        new Thread(() -> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(4000);
+                Socket socket = serverSocket.accept();
+
+                input = new DataInputStream(socket.getInputStream());
+                output = new DataOutputStream(socket.getOutputStream());
+
+                txtArea.appendText("Client connected...");
+
+                while (true) {
+                    String msg = input.readUTF();
+                    Platform.runLater(() -> txtArea.appendText("Client: " + msg + "\n"));
+
+                    if (msg.equalsIgnoreCase("exit")) {
+                        break;
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        ).start();
     }
 
+    @FXML
+    void sendbtnOnAction() {
+        String msg = txtmsg.getText();
+        try {
+            output.writeUTF(msg);
+            output.flush();
+            txtArea.appendText("Server: " + msg + "\n");
+            txtmsg.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
