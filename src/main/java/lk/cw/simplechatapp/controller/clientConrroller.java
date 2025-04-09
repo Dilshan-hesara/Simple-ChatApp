1293339a3a62563b6a68c6563e1479ffb1c4d56f
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-04-09 09:51:59 +0530

    client conect

--- src/main/java/lk/cw/simplechatapp/controller/clientConrroller.java
+++ src/main/java/lk/cw/simplechatapp/controller/clientConrroller.java
@@ -1,12 +1,19 @@
 package lk.cw.simplechatapp.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class clientConrroller {
+
 
     @FXML
     private Button sendbtn;
@@ -17,9 +24,43 @@ public class clientConrroller {
     @FXML
     private TextField txtmsg;
 
-    @FXML
-    void sendbtnOnAction(ActionEvent event) {
+    private DataInputStream input;
+    private DataOutputStream output;
 
+    public void initialize() {
+
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("localhost", 4000);
+                input = new DataInputStream(socket.getInputStream());
+                output = new DataOutputStream(socket.getOutputStream());
+
+                while (true) {
+                    String msg = input.readUTF();
+                    Platform.runLater(() -> txtArea.appendText("Server : "+ msg + "\n"));
+
+                    if (msg.equalsIgnoreCase("exit")) {
+                        break;
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    @FXML
+    void sendbtnOnAction() {
+        String msg = txtmsg.getText();
+        try {
+            output.writeUTF(msg);
+            output.flush();
+            txtArea.appendText("Client: " + msg + "\n");
+            txtmsg.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
